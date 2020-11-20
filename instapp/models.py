980b6ed9c0bd983e775d6988d9e4c7f24887d0d7
Jeff-Mwai <jeffmwai3@gmail.com>
2020-11-20 18:53:47 +0300

    Added image and profile methods

--- instapp/models.py
+++ instapp/models.py
@@ -1,14 +1,27 @@
 from django.db import models
 
 # Create your models here.
-class Image
+class Image(models.Model):
+    image = models.ImageField(upload_to='images/')
+    image_name = models.CharField(max_length=250, blank=True)
+    image_caption = models.CharField(max_length=250, blank=True)
+    user = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='posts')
+    likes = models.ManyToManyField(User, related_name='likes', blank=True, )
+    comments = models.CharField(max_length=250, blank=True)
 
 
+    @classproperty
+    def save_image(self):
+        self.save()
+
+    def delete_image(self):
+        self.delete()
+
+    def update_caption(self):
+        self.update()
 
 
-Image
-Image Name.
-Image Caption.
-Profile Foreign key
-Likes
-Comments
+class Profile(models.Model):
+    profile_photo = models.ImageField(upload_to='images/')
+    bio = models.CharField(max_length=250, blank=True)
+
