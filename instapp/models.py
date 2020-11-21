598d3111782d39dfe9adf10f20f257eb42897f8b
Jeff-Mwai <jeffmwai3@gmail.com>
2020-11-21 18:10:30 +0300

    Added forms.py

--- instapp/models.py
+++ instapp/models.py
@@ -4,8 +4,9 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 class Profile(models.Model):
-    profile_photo = models.ImageField(upload_to='images/')
-    bio = models.CharField(max_length=250, blank=True)
+    user_name = models.OneToOneField(User, on_delete=models.CASCADE,)
+    profile_picture = models.ImageField(upload_to='images/', default='image.jpg')
+    bio = models.TextField(max_length=500, default="My Bio", blank=True)
 
 class Image(models.Model):
     image = models.ImageField(upload_to='images/')
@@ -25,6 +26,10 @@ class Image(models.Model):
 
     def update_caption(cls, self):
         self.update()
+
+class POST(models.Model):
+    name = models.CharField(max_length = 30)
+    email = models.EmailField()
 
 
 
--- instapp/views.py
+++ instapp/views.py
@@ -1,7 +1,31 @@
 from django.shortcuts import render
 from django.http  import HttpResponse
+from .models import Image, Profile
+from django.contrib.auth.models import User
+
 
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
+
+def profile(request):
+    pass
+
+def new_post(request):
+    current_user = request.user
+   
+    if request.method == 'POST':
+        form = NewPostForm(request.POST, request.FILES)
+        if form.is_valid():
+            image = form.save(commit=False)
+            image.user = current_user
+           
+            image.save()
+            
+        return redirect('home')
+
+    else:
+        form = NewPostForm()
+    return render(request, 'new_post.html', {"form": form})
+
